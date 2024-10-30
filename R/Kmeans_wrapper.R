2c3b74d7944afba699ca98e5fcf97ae8028e49e1
Arpan Ghosh <arpan2001@tamu.edu>
2024-10-30 00:56:36 -0500

    myKmeans input check

--- R/Kmeans_wrapper.R
+++ R/Kmeans_wrapper.R
@@ -15,7 +15,18 @@ MyKmeans <- function(X, K, M = NULL, numIter = 100){
   n = nrow(X) # number of rows in X
   
   # Check whether M is NULL or not. If NULL, initialize based on K random points from X. If not NULL, check for compatibility with X dimensions.
-  
+  if(is.null(M)==TRUE){
+    Index<-sample(1:n,K,replace = FALSE)
+    M<-X[Index,]
+  }else
+  {
+    if(dim(M)[2]!=dim(X)[2] || dim(M)[1]!=K)
+    {
+      stop("M is not suitable")
+    }
+  }
+  #print(M)
+  M<-as.matrix(M)
   
   # Call C++ MyKmeans_c function to implement the algorithm
   Y = MyKmeans_c(X, K, M, numIter)
